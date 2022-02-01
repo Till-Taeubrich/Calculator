0f1f55ee534e60b4be5d60e26fab63c445cc83be
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-02-01 16:49:56 +1100

    seperate buttons

---
+++ script.js
@@ -0,0 +1,38 @@
+const one = document.querySelector('#calcArea');
+one
+
+let displayValue;
+
+function add(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+function divide(a, b) {
+  return a / b;
+}
+
+// function operate probably needs rework
+
+function operate(operator, Nr1, Nr2) {
+  if (operator === '+') {
+    add(Nr1, Nr2)
+  } else if (operator === '-') {
+    subtract(Nr1, Nr2)
+  } else if (operator === '*') {
+    multiply(Nr1, Nr2)
+  } else if (operator === '/') {
+    divide(Nr1, Nr2)
+  }
+}
+
