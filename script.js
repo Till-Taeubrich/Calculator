6ea55c4d361ffc2c4886b2c2b89d9e47f45f160e
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-02-05 10:26:24 +1100

    Calculation function added

--- script.js
+++ script.js
@@ -1,47 +1,93 @@
-const number = document.querySelectorAll('.numberButton');
+/* 
+    You’ll need to store the first number that is input into the calculator when a user presses an currentOperator,
+    and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
 
+    You should already have the code that can populate the display,
+    so once operate() has been called, update the display with the ‘solution’ to the operation.
 
-number.forEach(function(num) {
-  num.addEventListener('click', appendNumber)
-});
+    You need to figure out how to store all the values and call the operate function with them.
+ */
 
-let displayValue;
+// variables
+const display = document.querySelector('#display');
+const numberButtons = document.querySelectorAll('.numberButton');
+const operatorButtons = document.querySelectorAll('.operatorButton');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
+const equalsButton = document.querySelector('#equals');
 
+let currentDisplayValue;
+let firstOperand;
+let secondOperand;
+let currentOperator;
+let nextInputResetScreen = false;
 
+// functions
 function add(a, b) {
-  return a + b;
+  return (+a) + (+b);
 }
-
 
 function subtract(a, b) {
-  return a - b;
+  return (+a) - (+b);
 }
-
 
 function multiply(a, b) {
-  return a * b;
+  return (+a) * (+b);
 }
-
 
 function divide(a, b) {
-  return a / b;
+  return (+a) / (+b);
 }
 
-
-// function operate probably needs rework
-
-function operate(operator, Nr1, Nr2) {
-  if (operator === '+') {
-    add(Nr1, Nr2)
-  } else if (operator === '-') {
-    subtract(Nr1, Nr2)
-  } else if (operator === '*') {
-    multiply(Nr1, Nr2)
-  } else if (operator === '/') {
-    divide(Nr1, Nr2)
+function operate() {
+  if (currentOperator === '+') {
+    display.innerHTML = add(firstOperand, secondOperand)
+  } else if (currentOperator === '-') {
+    display.innerHTML = subtract(firstOperand, secondOperand)
+  } else if (currentOperator === '*') {
+    display.innerHTML = multiply(firstOperand, secondOperand)
+  } else if (currentOperator === '/') {
+    display.innerHTML = divide(firstOperand, secondOperand)
   }
 }
 
-function appendNumber(){
-  displayValue += num;
+function appendNumbers(){
+  if (display.innerHTML === '0') {
+    resetScreen();
+  }
+
+  if (nextInputResetScreen === true) {
+    resetScreen();
+    nextInputResetScreen = false;
+  }
+  currentDisplayValue = display.innerHTML += this.innerHTML;
 }
+
+function resetScreen(){
+  display.innerHTML = '';
+}
+
+function saveFirstNumber() {
+  firstOperand = currentDisplayValue;
+}
+
+function saveOperator(e) {
+  currentOperator = e.target.innerHTML;
+}
+
+// Events
+numberButtons.forEach(number => {
+  number.addEventListener('click', appendNumbers)
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+      saveFirstNumber(e);
+      saveOperator(e);
+      nextInputResetScreen = true;
+    })})
+
+equalsButton.addEventListener('click', () => {
+  secondOperand = currentDisplayValue;
+  operate()
+});
